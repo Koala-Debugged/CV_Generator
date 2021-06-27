35f4ab681638b7b2288e226e46829ed498193b11
Juan Antonio Sánchez Carmona <ja.sanchezcarmona@gmail.com>
2021-06-27 20:10:17 +0200

    Primera subida

---
+++ CV_Generator.Domain/CV/CvGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using iTextSharp.text.pdf;
+using CV_Generator.Domain.Model;
+
+namespace CV_Generator.Domain.CV
+{
+    class CvGenerator
+    {
+
+        /// <summary>
+        /// Este método genera el curriculum relleno mediante la plantilla que existe en el servidor.
+        /// </summary>
+        /// <param name="pathOrigen"></param>
+        /// <param name="pathDestino"></param>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        static bool GenerarCurriculum(string pathOrigen, string pathDestino, InfoDTO info)
+        {
+            bool flag = false;
+
+            string pathFinal = string.Format(pathOrigen + info.Empresa);
+
+            try
+            {
+
+                FileStream plantilla = new FileStream(pathOrigen, FileMode.Open);
+
+                using (var newFile = new FileStream(pathDestino, FileMode.Create))
+                {
+                    var pdfReader = new PdfReader(plantilla);
+
+                    var pdfStamper = new PdfStamper(pdfReader, newFile);
+
+                    AcroFields fields = pdfStamper.AcroFields;
+
+                    fields.SetField("Encabezado", info.Nombre);
+                    fields.SetField("PiePágina", new string("Descargado por " + info.Nombre + " " + info.Apellidos + "  para la empresa " + info.Email));
+                    fields.SetField("FechaYHora", DateTime.Now.ToString());
+
+                    pdfStamper.FormFlattening = true;
+
+                    plantilla.Close();
+                    pdfStamper.Close();
+                    pdfReader.Close();
+                }
+
+                flag = true;
+
+            }
+            catch (Exception ex)
+            {
+                flag = false;
+            }
+            finally
+            {
+
+            }
+
+            return flag;
+        }
+
+    }
+}
---
+++ CV_Generator.Domain/Model/InfoDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV_Generator.Domain.Model
+{
+    class InfoDTO
+    {
+        private string _nombre;
+        private string _apellidos;
+        private string _empresa;
+        private string _email;
+
+        public InfoDTO(string nombre, string apellidos, string empresa, string email)
+        {
+            this._nombre = nombre;
+            this._apellidos = apellidos;
+            this._empresa = empresa;
+            this._email = email;
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return this._apellidos; }
+            set { this._apellidos = value; }
+        }
+
+        public string Empresa
+        {
+            get { return this._empresa; }
+            set { this._empresa = value; }
+        }
+
+        public string Email
+        {
+            get { return this._email; }
+            set { this._email = value; }
+        }
+
+    }
+}
